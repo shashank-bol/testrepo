9b8e31e12849183a015138e197eae887bb3464a6
Shashank Singh <shashanksingh@Shashanks-MacBook-Pro.local>
2023-05-22 10:46:22 +0530

    h1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('<h1>Hello World</h1>');
 });
 
 server.listen(port, hostname, () => {
