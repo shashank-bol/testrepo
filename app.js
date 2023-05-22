2bb73c6ad49febaa3aeb61a9f08245af50fedc86
Shashank Singh <shashanksingh@Shashanks-MacBook-Pro.local>
2023-05-22 11:11:20 +0530

    express

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-const http = require('http');
+const express = require('express')
+const app = express() 
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/' , (req,res) =>{
+  res.send('<h1>Hello World</h1>')
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('<h1>Hello World</h1>');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000,()=> console.log('Server ruihni'))
