f3441506dbbf85b375161805e675064f867cc14e
shashlji <133962586+shashlji@users.noreply.github.com>
2023-05-22 15:30:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express() 
 const pool = require('./db')
-pool.connect()
+//pool.connect()
 app.use(express.json())
 app.get('/' , (req,res) =>{
   res.send('<h1>Hello World</h1>')
