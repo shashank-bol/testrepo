9854690d062f9a688e8cf29bed35c836758aff9d
shashlji <133962586+shashlji@users.noreply.github.com>
2023-05-22 12:06:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express() 
-
+const fs = require('fs')
+fs.writeFileSync("tets.txt" , "testing")
 app.get('/' , (req,res) =>{
   res.send('<h1>Hello World</h1>')
 })
