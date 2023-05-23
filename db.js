f320a70970c7d041468afe4eaf4583b3610f1ad0
shashlji <133962586+shashlji@users.noreply.github.com>
2023-05-23 14:45:59 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool 
 const config = require('dotenv').config()
 const pool = new Pool({
-    user: config.RDS_USERNAME || 'shash',
-    host: config.RDS_HOSTNAME || 'terraform-20230522095053294900000001.cbmcqryckfcu.ap-south-1.rds.amazonaws.com',
+    user: config.RDS_USERNAME || 'postgres',
+    host: config.RDS_HOSTNAME || 'testdb.cbmcqryckfcu.ap-south-1.rds.amazonaws.com',
     database: config.RDS_DB_NAME || 'shash_rds',
-    password: config.RDS_PASSWORD|| 'shash1234',
+    password: config.RDS_PASSWORD|| 'FRB8lYKyFcar3Nvpiw3N',
     port: config.RDS_PORT || 5432,
     
   })
